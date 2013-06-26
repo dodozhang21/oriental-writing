24ce34e255c7acb7aad23dd9d7f45160e20c61ca
Ying Zhang <dodozhang21@gmail.com>
2013-06-25 21:02:33 -0500

    undo some changes in header

--- header.php
+++ header.php
@@ -15,7 +15,6 @@
 <title><?php wp_title( '&raquo;', true, 'right' ); ?></title>
 <link rel="profile" href="http://gmpg.org/xfn/11" />
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
-<link href='http://fonts.googleapis.com/css?family=Molle:400italic&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
 <!--[if lt IE 9]>
 <script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
 <![endif]-->
